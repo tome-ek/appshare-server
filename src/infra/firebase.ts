a06a5a7d10f3e2f21940c52ac38498a8070e8602
tome-ek <crackerjack02@protonmail.com>
2021-09-16 16:13:34 +0200

    Firebase private key is now stores as a base64 string in the environment

--- src/infra/firebase.ts
+++ src/infra/firebase.ts
@@ -1,12 +1,15 @@
 import admin from 'firebase-admin';
 
 export const createFirebaseApp = (): void => {
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const buffer = Buffer.from(process.env.FIR_PRIVATE_KEY!, 'base64');
+
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIR_PROJECT_ID,
       clientEmail: process.env.FIR_CLIENT_EMAIL,
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      privateKey: process.env.FIR_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+      privateKey: buffer.toString('utf8').replace(/\\n/g, '\n'),
     }),
     storageBucket: process.env.FIR_STORAGE_BUCKET,
   });
